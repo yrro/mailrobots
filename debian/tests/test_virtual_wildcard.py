b3e5a3754e8947c0bed4d26f050e872dfe615f34
Sam Morris <sam@robots.org.uk>
2017-03-20 20:04:15 +0000

    Add test to ensure virtual accounts override aliases with the same local_part

--- debian/tests/test_virtual_wildcard.py
+++ debian/tests/test_virtual_wildcard.py
@@ -3,6 +3,7 @@ from pytest import *
 aliases_text = '''\
 alias: account
 *: account2
+account3: :fail: account should have precedence
 '''
 
 passwd_in_text = '''\
@@ -12,6 +13,8 @@ password = $6$CE0MQhhj0wGYjq2$jMtQd5En/oYHT7Uwbeb/IGuf1K9iWKIAUhHAIXVcwXJifC7gxT
 [user:account2]
 password = $6$CE0MQhhj0wGYjq2$jMtQd5En/oYHT7Uwbeb/IGuf1K9iWKIAUhHAIXVcwXJifC7gxT4al0BU0UE3aKtUiFXlYkC7cNW2ypohRmfz0.
 
+[user:account3]
+password = $6$CE0MQhhj0wGYjq2$jMtQd5En/oYHT7Uwbeb/IGuf1K9iWKIAUhHAIXVcwXJifC7gxT4al0BU0UE3aKtUiFXlYkC7cNW2ypohRmfz0.
 '''
 
 def test_alias_delivery(sendmail, imap, print_logs, print_journal):
@@ -33,3 +36,12 @@ def test_fallback_delivery(sendmail, imap, print_logs, print_journal):
         print_logs()
         print_journal()
 
+@mark.parametrize('imap_account', ['account3@test.example'])
+def test_account_overrides_alias(sendmail, imap, print_logs, print_journal):
+    try:
+        sendmail(To='account3@test.example', Subject='should be in mailbox')
+        imap.select()
+        assert ('OK', [b'1']) == imap.uid('search','subject "should be in mailbox"')
+    finally:
+        print_logs()
+        print_journal()
