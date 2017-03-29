2319c60215cfa5b16c195ad258a46b1720e1f1de
Sam Morris <sam@robots.org.uk>
2017-03-29 19:18:53 +0100

    Add spam filtering

--- debian/tests/test_clamav.py
+++ debian/tests/test_clamav.py
@@ -5,7 +5,7 @@ from pytest import *
 def test_clamav(sendmail, print_logs, print_journal):
     try:
         with raises(smtplib.SMTPDataError) as excinfo:
-            sendmail(To='account@test.example', Subject='clamav test', body=r'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*')
+            sendmail(To='account@test.example', Subject='clamav test', body='Virus message, should be blocked\r\n' r'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*' '\r\n')
         assert (550, b'Malware detected in message (Eicar-Test-Signature)') == excinfo.value.args
     finally:
         print_logs()
---
+++ debian/tests/test_spamassassin.py
@@ -0,0 +1,54 @@
+import email
+import smtplib
+
+from pytest import *
+
+@mark.xfail(reason='needs virtual alias rework', strict=True)
+def test_spamd_receives_no_mail(sendmail, print_logs, print_journal):
+    try:
+        with raises(smtplib.SMTPRecipientsRefused) as excinfo:
+            sendmail(To='spamd@spamd.invalid', Subject='spamd reject test', body='blah\r\n')
+        assert (550, 'Mail not accepted at this address') == excinfo.value.args[1]
+    finally:
+        print_logs()
+        print_journal()
+
+def test_obvious_spam_rejected(sendmail, print_logs, print_journal):
+    try:
+        with raises(smtplib.SMTPDataError) as excinfo:
+            sendmail(To='account@test.example', Subject='spamassassin test', body='Spam message, should be blocked\r\nXJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X\r\n')
+        assert 550 == excinfo.value.args[0]
+        assert b'Message classified as spam' in excinfo.value.args[1]
+    finally:
+        print_logs()
+        print_journal()
+
+@mark.xfail(strict=True)
+def test_large_messages_not_scanned(sendmail, print_logs, print_journal):
+    try:
+        assert 0
+    finally:
+        print_logs()
+        print_journal()
+
+@mark.parametrize('header', [
+    'x-spam-checker-version',
+    'x-spam-level',
+    'x-spam-status',
+    'x-spam-report',
+    'x-spam-asn',
+])
+def test_spam_headers_added(sendmail, imap, print_logs, print_journal, header):
+    try:
+        sendmail(To='account@test.example', Subject='spam header test', headers={header: 'should be removed'}, body='A sample message.\r\n')
+        imap.select()
+        status, uids = imap.uid('search', 'subject "spam header test"')
+        assert 'OK' == status
+        status, resp = imap.uid('fetch', uids[0].split()[0], '(rfc822)')
+        assert 'OK' == status
+        msg = email.message_from_bytes(resp[0][1])
+        assert 1 == len(msg.get_all(header))
+        assert 'should be removed' != msg[header]
+    finally:
+        print_logs()
+        print_journal()
