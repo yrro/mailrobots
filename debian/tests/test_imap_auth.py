f47779d32351eba31197eeed53aec0db1e51542d
Sam Morris <sam@robots.org.uk>
2017-03-30 13:31:58 +0100

    print_journal in imap_auth tests

--- debian/tests/test_imap_auth.py
+++ debian/tests/test_imap_auth.py
@@ -2,15 +2,24 @@ import imaplib
 
 from pytest import *
 
-def test_plain_logindisabled_capabilities(imap_preauth):
-    assert 'LOGINDISABLED' in imap_preauth.capabilities
+def test_plain_logindisabled_capabilities(imap_preauth, print_journal):
+    try:
+        assert 'LOGINDISABLED' in imap_preauth.capabilities
+    finally:
+        print_journal()
 
-def test_plain_login_privacy_required(imap_preauth):
-    with raises(imaplib.IMAP4.error) as excinfo:
-        imap_preauth.login('account@test.example', 'password')
-    assert excinfo.value.args[0].startswith(b'[PRIVACYREQUIRED]')
+def test_plain_login_privacy_required(imap_preauth, print_journal):
+    try:
+        with raises(imaplib.IMAP4.error) as excinfo:
+            imap_preauth.login('account@test.example', 'password')
+        assert excinfo.value.args[0].startswith(b'[PRIVACYREQUIRED]')
+    finally:
+        print_journal()
 
 @mark.usefixtures('user_mailbox')
-def test_tls_login(imap_preauth):
-    imap_preauth.starttls()
-    imap_preauth.login('account@test.example', 'password')
+def test_tls_login(imap_preauth, print_journal):
+    try:
+        imap_preauth.starttls()
+        imap_preauth.login('account@test.example', 'password')
+    finally:
+        print_journal()
